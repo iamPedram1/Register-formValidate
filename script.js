bedbd4dedb1a1e5e430a8e7c0655a42378735665
iamPedram1 <Pedram.pps737@Gmail.com>
2022-04-21 22:13:52 +0430

    Refactoring code, Debugging pswrd and email input, adding username validator

--- script.js
+++ script.js
@@ -1,59 +1,31 @@
-const button = document.querySelector(".btn").addEventListener("click", (e) => {
-  e.preventDefault();
+//////// Function
 
-  // Selecting Inputs Value
-  const inputEmail = document.querySelector("#inputEmail3").value;
-  const inputPassword = document.querySelector("#inputPassword3").value;
-  const inputConfirmPassword = document.querySelector("#inputPassword4").value;
-
-  // Check if Email is valid
-  if (!inputEmail.includes("@") || !inputEmail.endsWith(".com")) {
-    if (document.querySelectorAll(".email > .text-danger").length < 1)
-      newElm("Please enter a valid email address!", "text-danger", ".email");
-    // }
-  } else {
-    if (document.querySelectorAll(".email > .text-danger").length > 0) {
-      document.querySelector(".email > .text-danger").remove();
-    }
+// Remove From the dom Function
+function remover(querySelect) {
+  if (domSelectAll(querySelect).length > 0) {
+    document.querySelector(querySelect).remove();
   }
+}
 
-  // Check if passsword is more than 6 characther
-  if (inputPassword.length < 6) {
-    if (document.querySelectorAll(".password > .text-danger").length < 1) {
-      newElm(
-        "Password must be More than 6 Character",
-        "text-danger",
-        ".password"
-      );
-    }
-  } else {
-    if (document.querySelectorAll(".password > .text-danger").length > 0) {
-      document.querySelector(".password > .text-danger").remove();
-    }
-  }
+// Remove All Error Messages
+function errorMsgRemover(select) {
+  remover(`${select} > .text-danger`);
+  remover(`${select} > .text-success`);
+}
 
-  // Check if passwords are same
-  if (inputPassword == inputConfirmPassword) {
-    const spanFail = document.querySelectorAll(".confirm > .text-danger");
-    const spanSuccess = document.querySelectorAll(".confirm > .text-success");
-    if (spanSuccess.length < 1) {
-      if (spanFail.length > 0) {
-        spanFail[0].remove();
-      }
-      newElm("Confirm Password Match", "text-success", ".confirm");
-    }
-  } else {
-    const spanSucess = document.querySelectorAll(".confirm > .text-success");
-    const spanFail = document.querySelectorAll(".confirm > .text-danger");
-    if (spanFail.length < 1) {
-      if (spanSucess.length > 0) {
-        spanSucess[0].remove();
-      }
-      newElm("Confirm password dosen't match", "text-danger", ".confirm");
-    }
-  }
-});
+// Select All Function
+function domSelectAll(select) {
+  return document.querySelectorAll(select);
+}
 
+// Select Function
+function domSelect(select) {
+  return document.querySelector(select);
+}
+
+// ---------------------Validating Form--------------------------------- //
+
+// Create Element Function
 function newElm(text, className, parent) {
   const newSpan = document.createElement("span");
   const newText = document.createTextNode(text);
@@ -62,3 +34,75 @@ function newElm(text, className, parent) {
   const parentElement = document.querySelector(`${parent}`);
   parentElement.insertBefore(newSpan, null);
 }
+const button = domSelect(".btn").addEventListener("click", (e) => {
+  e.preventDefault();
+
+  // Selecting Inputs Value
+  const inputUsername = domSelect("#inputUsername3").value,
+    inputEmail = domSelect("#inputEmail3").value,
+    inputPassword = domSelect("#inputPassword3").value,
+    inputConfirmPassword = domSelect("#inputPassword4").value,
+    checkBox = domSelect("#gridCheck2").checked;
+
+  // Check if Username is more than 3 and less than 15 Character
+  if (inputUsername.length < 3) {
+    if (domSelectAll(".username > .text-danger").length < 1) {
+      newElm("Must be more than 3 character", "text-danger", ".username");
+    }
+  } else {
+    errorMsgRemover(".username");
+  }
+
+  if (inputUsername.length > 15) {
+    errorMsgRemover(".username");
+    if (domSelectAll(".username > .text-danger").length < 1) {
+      newElm("Must be less than 15 character", "text-danger", ".username");
+    }
+  }
+
+  // Check if Email is valid
+  if (!inputEmail.includes("@") || !inputEmail.endsWith(".com")) {
+    if (domSelectAll(".email > .text-danger").length < 1)
+      newElm("Please enter a valid email address!", "text-danger", ".email");
+    // }
+  } else {
+    errorMsgRemover(".email");
+  }
+
+  // Check if passsword is more than 6 characther
+  if (inputPassword.length <= 5) {
+    if (domSelectAll(".password > .text-danger").length < 1) {
+      newElm("Must be more than 6 character", "text-danger", ".password");
+    }
+  } else {
+    errorMsgRemover(".password");
+  }
+
+  // Check confirm input min and max character
+  if (inputConfirmPassword.length <= 5) {
+    if (domSelectAll(".confirm > .text-danger").length < 1) {
+      errorMsgRemover(".confirm");
+      newElm("Must be more than 6 character", "text-danger", ".confirm");
+    }
+  }
+
+  if (inputConfirmPassword.length > 30) {
+    // errorMsgRemover(".confirm");
+    newElm("Must be less than 30 character", "text-danger", ".confirm");
+  }
+
+  // Check if passwords are same
+  if (inputConfirmPassword.length > 5) {
+    if (inputPassword == inputConfirmPassword) {
+      if (domSelectAll(".confirm > .text-success").length < 1) {
+        errorMsgRemover(".confirm");
+        newElm("Confirm Password Match", "text-success", ".confirm");
+      }
+    } else {
+      errorMsgRemover(".confirm");
+      if (domSelectAll(".confirm > .text-danger").length < 1) {
+        newElm("Confirm password does not match", "text-danger", ".confirm");
+      }
+    }
+  }
+});
